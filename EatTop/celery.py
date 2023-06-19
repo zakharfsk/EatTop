ce489310bd3dcaaf1337bcaf1154814d8ac3a6a9
Zakhar Fesik <fesikzakhar@khmnu.edu.ua>
2023-06-19 21:14:27 +0300

    update celery.py config

--- EatTop/celery.py
+++ EatTop/celery.py
@@ -4,5 +4,4 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EatTop.settings")
 app = Celery("EatTop")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.control.purge()
 app.autodiscover_tasks()
